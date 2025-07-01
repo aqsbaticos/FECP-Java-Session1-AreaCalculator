90128d6440d71980a6460588c9bf4af66469e7bd
Andric Quinn Baticos <andric.baticos@GXCHLAP00696.local>
2025-07-01 16:23:53 +0800

    Final Submission, Area Calculator

---
+++ README.md
@@ -0,0 +1,7 @@
+# Lab 4: Area Calculator
+Developed by: Baticos, Andric Quinn S.
+
+### Screenshot of Proof
+![Screenshot Proof](/proof1.png?raw=true "proof")
+![Screenshot Proof](/proof2.png?raw=true "proof")
+![Screenshot Proof](/proof3.png?raw=true "proof")
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,68 @@
 package org.example;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
 public class Main {
+
+    static double calculateCircleArea(double radius) {
+        return (double) Math.PI * (radius*radius);
+    }
+
+    static double calculateTriangleArea(double base, double height) {
+        return (base * height) / 2;
+    }
+
+    static double calculateRectangleArea(double length, double width) {
+        return length * width;
+    }
+
     public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
+        String shapeChoice = ""; double area = 0.0;
+        boolean isValid = false;
+
+        System.out.println("=== Area Calculator ===");
+        System.out.println("1. Circle");
+        System.out.println("2. Triangle");
+        System.out.println("3. Rectangle");
+        System.out.print("Choose a shape (1-3): ");
+        int choice = in.nextInt();
+
+        switch(choice) {
+            case 1:
+                shapeChoice = "circle";
+                System.out.print("Enter the radius: ");
+                double radius = in.nextDouble();
+                area = calculateCircleArea(radius);
+                isValid = true;
+                break;
+            case 2:
+                shapeChoice = "triangle";
+                System.out.print("Enter the base: ");
+                double base = in.nextDouble();
+                System.out.print("Enter the height: ");
+                double height = in.nextDouble();
+                area = calculateTriangleArea(base, height);
+                isValid = true;
+                break;
+            case 3:
+                shapeChoice = "rectangle";
+                System.out.print("Enter the radius: ");
+                double length = in.nextDouble();
+                System.out.print("Enter the radius: ");
+                double width = in.nextDouble();
+                area = calculateRectangleArea(length, width);
+                isValid = true;
+                break;
+            default:
+                System.out.println("Invalid choice.");
+                break;
+        }
+
+        if (isValid) {
+            System.out.print("Area of the " + shapeChoice + ": ");
+            System.out.printf("%.2f\n", area);
+        }
     }
 }
